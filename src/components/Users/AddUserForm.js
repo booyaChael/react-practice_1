9dbf1a4b7158ecd5d6e58940388f63d3fcd4b260
booyaChael <chaeeul2022@gmail.com>
2023-04-23 18:21:35 +0900

    input validation

--- src/components/Users/AddUserForm.js
+++ src/components/Users/AddUserForm.js
@@ -16,6 +16,12 @@ const AddUserForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (+enteredAge < 1) {
+      return;
+    }
     setEnteredUsername("");
     setEnteredAge("");
   };
