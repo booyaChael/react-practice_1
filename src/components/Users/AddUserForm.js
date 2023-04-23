96a30ea2246c293318ee45eba1a9cf4356ad6947
booyaChael <chaeeul2022@gmail.com>
2023-04-23 18:01:54 +0900

    Card UI 만들어 AddUserForm에 적용

--- src/components/Users/AddUserForm.js
+++ src/components/Users/AddUserForm.js
@@ -1,15 +1,15 @@
 import classes from "./AddUserForm.module.css";
-
+import Card from "../UI/Card";
 const AddUserForm = () => {
   return (
-    <>
+    <Card>
       <form className={classes.input}>
         <label htmlFor={"username"}>Username</label>
         <input id="username" type="text" />
         <label htmlFor={"age"}>Age</label>
         <input id="age" type="number" />
       </form>
-    </>
+    </Card>
   );
 };
 
