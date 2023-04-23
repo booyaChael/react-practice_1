169c234564c4814151262de2443556ac528fd118
booyaChael <chaeeul2022@gmail.com>
2023-04-23 18:19:46 +0900

    input 감지 및 제출 시 input 비우기

--- src/components/Users/AddUserForm.js
+++ src/components/Users/AddUserForm.js
@@ -1,14 +1,42 @@
 import classes from "./AddUserForm.module.css";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
+import { useState } from "react";
 const AddUserForm = () => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
+  const usernameChangeHandler = (e) => {
+    setEnteredUsername(e.target.value);
+  };
+
+  const ageChangeHandler = (e) => {
+    setEnteredAge(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setEnteredUsername("");
+    setEnteredAge("");
+  };
+
   return (
     <Card>
-      <form className={classes.input}>
+      <form className={classes.input} onSubmit={handleSubmit}>
         <label htmlFor={"username"}>Username</label>
-        <input id="username" type="text" />
+        <input
+          id="username"
+          type="text"
+          onChange={usernameChangeHandler}
+          value={enteredUsername}
+        />
         <label htmlFor={"age"}>Age</label>
-        <input id="age" type="number" />
+        <input
+          id="age"
+          type="number"
+          onChange={ageChangeHandler}
+          value={enteredAge}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
